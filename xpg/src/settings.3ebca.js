ebd2184d4935e29344da3a6c29281ff5cc84355f
ZhouJun2303 <478103223@qq.com>
2019-11-26 08:47:15 +0800

    xpg

---
+++ xpg/src/settings.3ebca.js
@@ -0,0 +1,184 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "84yZ5dtfBK8aJNPsEdFo12": [
+                "res/Texture/b1.png",
+                0
+            ],
+            "29l8Z66KlKWaCvWumDMg2z": [
+                "res/Texture/bg_timer.png",
+                0
+            ],
+            "c5L4VZOrhJq6xkeKhTzL6+": [
+                "res/Texture/dici .png",
+                0
+            ],
+            "c6Y0twBpRFbaPsoNAJDwj7": [
+                "res/Texture/icon_energy.png",
+                0
+            ],
+            "26aTDt18FLpKsbgyJcL+9n": [
+                "res/Texture/indexBg.png",
+                0
+            ],
+            "e1Cg1ctV9JHYj3KFYQ6e4F": [
+                "res/Texture/love.png",
+                0
+            ],
+            "c20/oSRMBOhaVQ+fDxb4eY": [
+                "res/Texture/lovebg.png",
+                0
+            ],
+            "701AWTq5hO/KzIcaLRiR9z": [
+                "res/Texture/normal.png",
+                0
+            ],
+            "a7YzIKTtFN+bWqPBsbcJAS": [
+                "res/Texture/sky.png",
+                0
+            ],
+            "83GEtdbZJIyKYoNkamryRW": [
+                "res/Texture/start_1.png",
+                0
+            ],
+            "11y7PJmTZPtY+XUYd6xNWz": [
+                "res/Texture/timer_bar.png",
+                0
+            ],
+            "eaWh0obIdOEpBUfVwgbTqO": [
+                "res/Texture/timer_slot.png",
+                0
+            ],
+            "65Gk/TB+FC2q/Rq/oOFnwF": [
+                "res/Texture/title.png",
+                0
+            ],
+            "26d4n7auRJrJKw98thLBEd": [
+                "res/Texture/wall.png",
+                0
+            ],
+            "19KFzpsY1NJ7q0TBO90b8w": [
+                "res/font/mikado_outline_shadow.png",
+                0
+            ]
+        },
+        internal: {
+            "71VhFCTINJM6/Ky3oX9nBT": [
+                "image/default_btn_disabled.png",
+                0
+            ],
+            "e8Ueib+qJEhL6mXAHdnwbi": [
+                "image/default_btn_normal.png",
+                0
+            ],
+            "b4P/PCArtIdIH38t6mlw8Y": [
+                "image/default_btn_pressed.png",
+                0
+            ],
+            "b2aHrECZ5APKGS/0d2hvT1": [
+                "particle/atom.plist",
+                1
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.Texture2D",
+        "cc.ParticleAsset"
+    ],
+    launchScene: "db://assets/scene/welcom.fire",
+    scenes: [
+        {
+            url: "db://assets/scene/welcom.fire",
+            uuid: 3
+        },
+        {
+            url: "db://assets/scene/gameover.fire",
+            uuid: 0
+        },
+        {
+            url: "db://assets/scene/game.fire",
+            uuid: 2
+        }
+    ],
+    packedAssets: {
+        "025aa253a": [
+            1,
+            "45mMWUMEZGX6Y7aCTs79nP",
+            4,
+            "a0YZ5G0plBtJFmzRNeSasj",
+            0
+        ],
+        "03e5c5ca3": [
+            "a9d4ZU+g1J/Y8Jug1YgPdl",
+            "f52SrHVVFGmbxXo9WFbH8g"
+        ],
+        "04600283b": [
+            "0dXk1mRi9DJLXNp08gDM66",
+            "27IrXajz1ISJkMwD/9o4Rc",
+            1,
+            "2c6S9WFYRGBptw5uh5G6vU",
+            "38GiS3zjRC2a29CCQB1FFO",
+            "607+lVcLdE15XyhGKyfjA/",
+            "87NSwtV79J1p27n4v9MYe5",
+            "8fMaaTqHRMWq8tlNG8CLCj",
+            "bfDv0aaptDaaGekqjheyd7",
+            2,
+            "c63kIDyLNMxaF8FUjOt+rC",
+            "ce5A3zSkhHGI9bNP7i3pDo",
+            "d7zjmNMopCYrggafVTGZ9I",
+            "dfWLnMgLJJAIsJ8iRhtKp4",
+            "e97GVMl6JHh5Ml5qEDdSGa",
+            "f0BIwQ8D5Ml7nTNQbh1YlS",
+            "fcIs1rJzFLHrMCLpEe3jCS"
+        ],
+        "0eeeaa1d": [
+            3,
+            "37w4C/N35KUKWh+JXUhBiU",
+            4,
+            "60WGn/oRFO4baRZKko6wbv",
+            "74c16z311HS6BSlDaaHopu"
+        ]
+    },
+    orientation: "portrait",
+    uuids: [
+        "ddETVoHUNApKfCOn7aT/gQ",
+        "29FYIk+N1GYaeWH/q1NxQO",
+        "c5/LX+tU1GF4Hilh29FEhl",
+        "14vXReOxBKGof5RmFbE9mA",
+        "47w75UvYFNI4iuv6AkEtIi"
+    ],
+    "md5AssetsMap": {
+        "02/025aa253a.json": "6cbd6",
+        "03/03e5c5ca3.json": "a2bf2",
+        "04/04600283b.json": "160b3",
+        "0e/0eeeaa1d.json": "4051b",
+        "assets/res/font/mikado_outline_shadow.png": "c1880",
+        "assets/res/Texture/b1.png": "1d439",
+        "assets/res/Texture/bg_timer.png": "8e1f8",
+        "assets/res/Texture/dici .png": "e37be",
+        "assets/res/Texture/icon_energy.png": "36feb",
+        "assets/res/Texture/indexBg.png": "623b7",
+        "assets/res/Texture/love.png": "6c11d",
+        "assets/res/Texture/lovebg.png": "fd905",
+        "assets/res/Texture/normal.png": "8e218",
+        "assets/res/Texture/sky.png": "84602",
+        "assets/res/Texture/start_1.png": "b4598",
+        "assets/res/Texture/timer_bar.png": "337e8",
+        "assets/res/Texture/timer_slot.png": "1dbb5",
+        "assets/res/Texture/title.png": "cdf3f",
+        "assets/res/Texture/wall.png": "bef0b",
+        "internal/image/default_btn_disabled.png": "286c6",
+        "internal/image/default_btn_normal.png": "1ecb7",
+        "internal/image/default_btn_pressed.png": "bedf4",
+        "internal/particle/atom.plist": "e31bf"
+    }
+};
